b9d89928a00cce5c3f42e808b66e9a5aac5c2f6d
David <davidwilliamhanley@gmail.com>
2022-04-02 16:16:58 -0600

    fetchData setup

--- controller/FetchData.js
+++ controller/FetchData.js
@@ -1,11 +1,16 @@
-import { api_query, image_api } from "../model/API_data";
-
 export const fetchQuery = async (query) => {
+    const api_query = `https://api.artic.edu/api/v1/artworks/search?q=${query}&page=1&limit=10&fields=id,title,image_id,date_display,artist_display,place_of_origin,medium_display,color`
     try {
         const response = await fetch(api_query);
         const json = await response.json();
+        console.log(json);
         return json;
     } catch (error) {
         console.log(error);
     }
+}
+
+export const getImage = (image_id) => {
+    image_id;
+    return image_api;
 }
--- view/ArtSearch.js
+++ view/ArtSearch.js
@@ -1,17 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { View, Text, FlatList, StyleSheet, Image, Dimensions, Button, Animated } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import { fetchQuery } from '../controller/FetchData';
 
 const window = Dimensions.get("window");
 const screen = Dimensions.get("screen");
 const ArtSearch = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+    var json_data;
     return (
-        <View>
-        <Searchbar/>
-        
-        <View style={styles.container}>            
-            <Text>{'Claude Monet\nFrench, 1840-1926'}</Text>
-        </View>
+        <View style={styles.container}>
+            <Searchbar
+                style={styles.searchbar}
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+                onIconPress={() => {json_data = fetchQuery(searchQuery);}}
+                onSubmitEditing={() => {json_data = fetchQuery(searchQuery);}}
+            />
+            <FlatList
+            data={json_data}
+            renderItem={({item}) => (
+                <View></View>
+            )}
+            />
+            
         </View>
     )
 };
@@ -19,26 +33,18 @@ const ArtSearch = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         backgroundColor: 'hsl(197, 37%, 41%)',
-        padding: 80,
+        padding: 20,
         alignItems: 'center',
         textAlign: 'center',
+    },
+    searchbar: {
+        marginTop: 100
     },
     text: {
         color: '#000',
         fontSize: 26,
-    },
-    image: {
-        resizeMode: 'contain',
-        aspectRatio: 1,
-        width: window.width * 0.99,
-    },
-    buttons: {
-        flexBasis: 100,
-        justifyContent: 'space-evenly',
-        marginVertical: 20,
-    },
+    },    
 });
 
 export default ArtSearch;
